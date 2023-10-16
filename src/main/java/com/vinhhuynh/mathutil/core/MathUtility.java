7c6db5ada15e5a76f3ea3aff916a7de1b6d63c6c
huynhlephcvinh <phucvinh710@gmail.com>
2023-10-16 11:26:35 +0700

    Update getf() to use recursion and to see gree4ever

--- src/main/java/com/vinhhuynh/mathutil/core/MathUtility.java
+++ src/main/java/com/vinhhuynh/mathutil/core/MathUtility.java
@@ -20,20 +20,50 @@ public class MathUtility {
     // n! tăng rất nhanh, 20! vừa đủ full kiểu long 18 con số 0
     // vậy 21! ko chứa trong long dc, tràn kiểu dữ liệu
     // quy ước < 0 ! và > 20! ném ngoại lệ, ko tính dc
-    public static long getFactorial(int n) {
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("Invalid n. n must be 0..20");
+//        
+//        if (n == 0 || n == 1)
+//            return 1; // kết thúc sớm hàm, nếu có thể
+//        
+//        long result = 1; // biến tích lũy, gom dồn việc nhân, biến con heo đất
+//                         // acc accumlation
+//        
+//        // sống sót đến for, sure n = 2..20                 
+//        for (int i = 2; i <= n; i++) 
+//            result *= i;
+//               
+//        return result;
+//    }
+    
+    
+    //Dùng đê quy
+    //5! =1.2.3.4   .5
+    //5! =      4!*5 = 5*4!
+    //4! 1.2.3   .4
+    //        3! *4 = 4*3!
+    //3!=3*2!
+    //2! = 2*1! dừng nhen, đệ quy luôn phải dừng
+    //N! = N*(N-1)!
+    
+        public static long getFactorial(int n) {  //n!
         if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid n. n must be 0..20");
         
         if (n == 0 || n == 1)
             return 1; // kết thúc sớm hàm, nếu có thể
-        
-        long result = 10; // biến tích lũy, gom dồn việc nhân, biến con heo đất
-                         // acc accumlation
-        
-        // sống sót đến for, sure n = 2..20                 
-        for (int i = 2; i <= n; i++) 
-            result *= i;
-               
-        return result;
+        // Dùng đệ quy
+        return n*getFactorial(n-1);
+           
     }
+        
+        //KĨ THUẬT KIỂM THỬ GỌI LÀ: REGRESSION TESTING
+        //                           KIỂM THỬ HỒI QUY  - TEST LẠI NHỮNG THỨ ĐÃ TỪNG TEST
+        //Trong thực tế làm dự án, code của ta sẽ liên tục đc chỉnh sửa
+        //sửa vì:
+        // fix bug thì phải sửa vode, sửa hàm
+        // thêm hàm, thêm tính năng
+        // tối ưu hàm cũ gọn hơn dễ đọc hơn, chạy nhanh hơn
+        //Hàm đang ngon - màu xanh, nếu ta sửa hàm vì lí do nào đó
 }
